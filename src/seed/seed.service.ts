afac9e6d420d14b55d657632d24a851a894aecf3
Jhon Bellido <jhonbellidovargas@gmail.com>
2023-05-16 10:12:03 -0500

    segunda forma de insertar

--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -13,16 +13,22 @@ export class SeedService {
   private readonly axios: AxiosInstance = axios;
 
   async executeSeed() {
+    await this.pokemonModel.deleteMany({});
     const { data } = await this.axios.get<PokeResponse>(
       'https://pokeapi.co/api/v2/pokemon?limit=15',
     );
-
+    const insertPromisesArray = [];
     data.results.forEach(async ({ name, url }) => {
       const segments = url.split('/');
       const no: number = +segments[segments.length - 2];
 
-      const pokemon = await this.pokemonModel.create({ name, no });
+      // const pokemon = await this.pokemonModel.create({ name, no });
+      insertPromisesArray.push(this.pokemonModel.create({ name, no }));
     });
+
+    // Espera todas as promises serem resolvidas
+    await Promise.all(insertPromisesArray);
+
     return 'Seed executed';
   }
 }
