28e730d4d317b4e13e1aa9ab7a2d2edb77dda750
Jhon Bellido <jhonbellidovargas@gmail.com>
2023-05-16 09:26:01 -0500

    antes de tarea

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,7 @@ import { join } from 'path';
 import { MongooseModule } from '@nestjs/mongoose';
 import { PokemonModule } from './pokemon/pokemon.module';
 import { CommonModule } from './common/common.module';
+import { SeedModule } from './seed/seed.module';
 
 @Module({
   imports: [
@@ -13,6 +14,7 @@ import { CommonModule } from './common/common.module';
     MongooseModule.forRoot('mongodb://localhost:27017/nest-pokemon'),
     PokemonModule,
     CommonModule,
+    SeedModule,
   ],
 })
 export class AppModule {}
---
+++ src/seed/seed.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@nestjs/common';
+import axios, { AxiosInstance } from 'axios';
+import { PokeResponse } from './interfaces/poke-reponse.inteface';
+
+@Injectable()
+export class SeedService {
+  private readonly axios: AxiosInstance = axios;
+
+  async executeSeed() {
+    const { data } = await this.axios.get<PokeResponse>(
+      'https://pokeapi.co/api/v2/pokemon?limit=15',
+    );
+
+    data.results.forEach(({ name, url }) => {
+      const segments = url.split('/');
+      const no: number = +segments[segments.length - 2];
+      console.log({ name, no });
+    });
+    return data.results;
+  }
+}
