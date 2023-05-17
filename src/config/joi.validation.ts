dab50814d52dfec58f4c57c48f6f80761fb3c403
Jhon Bellido <jhonbellidovargas@gmail.com>
2023-05-17 09:22:57 -0500

    despliegue final

--- src/config/joi.validation.ts
+++ src/config/joi.validation.ts
@@ -2,6 +2,7 @@ import * as Joi from 'joi';
 
 export const JoiValidationSchema = Joi.object({
   // NODE_ENV: Joi.string().valid('dev', 'prod', 'test', 'stage').default('dev'),
+  
   MONGODB: Joi.string().required(),
   PORT: Joi.number().default(3005),
   DEFAULT_LIMIT: Joi.number().default(6),
