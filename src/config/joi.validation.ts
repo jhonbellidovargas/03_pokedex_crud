786baca5258982eed799c0a8947399f2073bc0d9
Jhon Bellido <jhonbellidovargas@gmail.com>
2023-06-05 13:56:39 -0500

    Dockerizar aplicacion config

--- src/config/joi.validation.ts
+++ src/config/joi.validation.ts
@@ -2,7 +2,6 @@ import * as Joi from 'joi';
 
 export const JoiValidationSchema = Joi.object({
   // NODE_ENV: Joi.string().valid('dev', 'prod', 'test', 'stage').default('dev'),
-  
   MONGODB: Joi.string().required(),
   PORT: Joi.number().default(3005),
   DEFAULT_LIMIT: Joi.number().default(6),
